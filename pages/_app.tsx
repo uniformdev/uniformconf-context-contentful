4d754e453e23a6f232e817b490b22f8fc8b42eee
Alex Shyba <57730703+uniform-demo@users.noreply.github.com>
2022-06-21 15:35:39 -0700

    feat: enable vercel middleware

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
-import { UniformContext } from '@uniformdev/context-react';
-import type { UniformAppProps } from '@uniformdev/context-next';
-import { createUniformContext } from '../lib/uniformContext';
-import '../styles/globals.css';
+import { UniformContext } from "@uniformdev/context-react";
+import type { UniformAppProps } from "@uniformdev/context-next";
+import { createUniformContext } from "../lib/uniformContext";
+import "../styles/globals.css";
 
 const clientContext = createUniformContext();
 
-function UniformContextApp({ Component, pageProps, serverUniformContext }: UniformAppProps) {
+function UniformContextApp({
+  Component,
+  pageProps,
+  serverUniformContext,
+}: UniformAppProps) {
   return (
-    <UniformContext context={serverUniformContext ?? clientContext}>
+    <UniformContext
+      context={serverUniformContext ?? clientContext}
+      outputType="edge"
+    >
       <Component {...pageProps} />
     </UniformContext>
   );
